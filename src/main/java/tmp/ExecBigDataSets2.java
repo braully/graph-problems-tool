a47106e00fd21cea62557235385b4853dc34c2b6
strike <strike@alastor-note>
2023-01-22 21:15:14 -0300

    Experimentos heuristicos

--- src/main/java/tmp/ExecBigDataSets2.java
+++ src/main/java/tmp/ExecBigDataSets2.java
@@ -186,7 +186,7 @@ public class ExecBigDataSets2 {
             //            "Livemocha",
             //            "BlogCatalog",
             //            "BuzzNet", 
-            "Last.fm",
+//            "Last.fm",
             "YouTube2"
         };
 //        GraphHullNumberHeuristicV5Tmp heur = new GraphHullNumberHeuristicV5Tmp();
@@ -230,7 +230,7 @@ public class ExecBigDataSets2 {
         File resultFile = new File(strResultFile);
         BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile, true));
 
-        for (int k = 4; k <= 10; k++) {
+        for (int k = 2; k <= 10; k++) {
             heur1.K = heur2.K = heur3.K
                     = heur4.K = heur5.K = heur5t.K = heur5t2.K = tss.K = k;
             System.out.println("-------------\n\nK: " + k);
